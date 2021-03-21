f3b610195786d08c6a9e43a7b3ca876e2c155d04
Michael Truong <truong.michael.2013@gmail.com>
2021-03-21 12:50:58 -0400

    Create Header.js component; add temporary styling to App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet} from 'react-native';
 
 // Note: Functions just like React 'Styled-Components'
 const styles = StyleSheet.create({
@@ -19,6 +19,12 @@ const styles = StyleSheet.create({
   text: {
     color: 'darkslateblue', 
     fontSize: 30
+  },
+  // Note: React-Native 'styles' attribute doesn't take units (e.g. px, em), just 100
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: (100/2)
   }
 })
 
@@ -30,7 +36,7 @@ const App = () => {
       <Text style={styles.text}>
         Hello World
       </Text>
-      
+      <Image source={{uri: 'https://www.randomuser.me/api/portraits/men/3.jpg'}} style={styles.img}/> 
     </View>
   )
 }

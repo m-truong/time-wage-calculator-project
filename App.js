7b8c1cb953ea870c6984d7810f2d27f095486978
Michael Truong <truong.michael.2013@gmail.com>
2021-03-21 12:41:43 -0400

    Add StyleSheet react-native component for modular, reusable CSS styles

--- App.js
+++ App.js
@@ -7,17 +7,30 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+
+// Note: Functions just like React 'Styled-Components'
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  text: {
+    color: 'darkslateblue', 
+    fontSize: 30
+  }
+})
 
 // Note: In React-Native, must incase everything in a native <View> component containers 
 
-
 const App = () => {
   return (
-    <View>
-      <Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
         Hello World
       </Text>
+      
     </View>
   )
 }

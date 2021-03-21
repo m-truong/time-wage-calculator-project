877f1f3521227962a338192b494c8ad4ebd6cf5d
Michael Truong <truong.michael.2013@gmail.com>
2021-03-21 15:10:56 -0400

    Create FlatList component for App.js with useState

--- App.js
+++ App.js
@@ -7,36 +7,54 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet} from 'react-native';
+import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+// import { uuid } from 'uuidv4';
 
 // Note: Functions just like React 'Styled-Components'
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
+  container: {
+    flex: 1,
+    // assign paddingTop 60px = height of <Header> component to clear the top
+    paddingTop: 60,
+    // justifyContent: 'center', 
+    // alignItems: 'center'
   },
   text: {
-    color: 'darkslateblue', 
+    color: 'black',
     fontSize: 30
   },
   // Note: React-Native 'styles' attribute doesn't take units (e.g. px, em), just 100
   img: {
     width: 100,
     height: 100,
-    borderRadius: (100/2)
+    // Gives rounded borders
+    borderRadius: (100 / 2)
   }
 })
 
 // Note: In React-Native, must incase everything in a native <View> component containers 
 
 const App = () => {
+  const [items, setItems] = useState([
+    { id: Math.random(), text: 'Milk' },
+    { id: Math.random(), text: 'Coffee' },
+    { id: Math.random(), text: 'Bagel' },
+    { id: Math.random(), text: 'Bread' },
+  ])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Hello World
-      </Text>
-      <Image source={{uri: 'https://www.randomuser.me/api/portraits/men/3.jpg'}} style={styles.img}/> 
+      {/* Note: Using 'props' to pass-down values makes components very reusable and modular. */}
+      <Header title={"Time-2-Wage Calculator"} />
+      <FlatList
+        // Tip: hard-coded 'data' array property on <FlatList> component
+        data={items}
+        // Tip: renderItem hard-coded function 
+        renderItem={({ item }) => <ListItem item={item}/>}
+      />
+      <Image source={{ uri: 'https://www.randomuser.me/api/portraits/men/3.jpg' }} style={styles.img} />
     </View>
   )
 }
---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+// Note: Functions just like React 'Styled-Components'
+const styles = StyleSheet.create({
+    header: {
+        height: 60,
+        padding: 15,
+        backgroundColor: '#8908B6',
+    },
+    text: {
+        color: '#fff',
+        fontSize: 23,
+        textAlign: 'center'
+    }
+})
+
+// Note: In React-Native, must incase everything in a native <View> component containers 
+
+const Header = ({ title }) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.text}>{title}</Text>
+        </View>
+    )
+}
+
+// Tip: Assign defaultProps to <Header> component 
+// If no props passed down, from topmost component, all {attributes} default 
+    // to values on this object
+Header.defaultProps = {
+    title: 'Default Title'
+}
+
+export default Header;

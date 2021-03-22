6ffde349d575c5fde86d3f103e30c3549cc10efb
Michael Truong <truong.michael.2013@gmail.com>
2021-03-22 10:42:14 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,57 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler'
 import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, FlatList, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import Header from './components/Header';
+import { WelcomeScreen } from './screens/WelcomeScreen';
 
-// Note: Functions just like React 'Styled-Components'
+// Creates Stack
+const Stack = createStackNavigator();
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // assign paddingTop 60px = height of <Header> component to clear the top
     paddingTop: 60,
-    // justifyContent: 'center', 
-    // alignItems: 'center'
   },
   text: {
     color: 'black',
     fontSize: 30
   },
-  // Note: React-Native 'styles' attribute doesn't take units (e.g. px, em), just 100
   img: {
     width: 100,
     height: 100,
-    // Gives rounded borders
     borderRadius: (100 / 2)
   }
 })
 
-// Note: In React-Native, must incase everything in a native <View> component containers 
-
 const App = () => {
-
-
-  const deleteItemHandler = (id) => {
-
-  }
-
-  const addItemHandler = (text) => {
-
-  }
-
+  
   return (
-    <View style={styles.container}>
-      {/* Note: Using 'props' to pass-down values makes components very reusable and modular. */}
-      <Header title={"Time-2-Wage Calculator"} />
-
-      <Image source={{ uri: 'https://www.randomuser.me/api/portraits/men/30.jpg' }} style={styles.img} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={WelcomeScreen}
+          options={{ title: 'Welcome' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export const WelcomeScreen = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}

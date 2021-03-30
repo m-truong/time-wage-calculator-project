6ddc7141d66cea0388ac1fcf68ec9911e970c5cc
Michael Truong <truong.michael.2013@gmail.com>
2021-03-30 15:55:59 -0400

    Calculator successfully computes and converts hourly wage and item expense to days, hours, and minutes rounded to significant figures

--- components/CalculateButton.js
+++ components/CalculateButton.js
@@ -34,11 +34,11 @@ const CalculateButton = ({ text, calculate }) => {
         let mins = 0;
         let minsDiffDec = 0;
         // Secs?
-        
+
         // If conditional to filter decimal using .join.split
-        const value = (priceExpense / hourlyWage)
+        const hoursToEarn = (priceExpense / hourlyWage)
         // .toFixed() rounds to 2 significant figures
-        const roundedSigHours = value.toFixed(2)
+        const roundedSigHours = hoursToEarn.toFixed(2)
         console.log(`The roundedSigFigs is ${roundedSigHours}`)
         // Complex calculator state to determine days
         // roundDays(roundedSigHours)
@@ -46,26 +46,41 @@ const CalculateButton = ({ text, calculate }) => {
         // If roundedSigHours > 24 // ** Only executes if hours over 24
         if (roundedSigHours > 24) {
             // take Difference 
-            days = Math.trunc(roundedSigHours/24)
+            days = Math.trunc(roundedSigHours / 24)
             console.log(`The number of days is ${days}`)
-            hoursDiffDec = (roundedSigHours/24) - days
+            hoursDiffDec = (roundedSigHours / 24) - days
             console.log(`The number of hours remaining is ${hoursDiffDec}`)
             // then multiple by 24hrs/day
-            hoursDiff = hoursDiffDec*24
+            hoursDiff = hoursDiffDec * 24
+
+
+            // subtracting gives mins** 
+            minsDiffDec = hoursDiff - Math.trunc(hoursDiff)
+            // convert into 60mins/sec
+            mins = minsDiffDec * 60
+
+            // Truncate the mins 
+            mins = Math.trunc(mins)
+
+            // Truncate the hours
             hoursDiff = Math.trunc(hoursDiff)
             console.log(`The number of hours remaining CONVERTED is ${hoursDiff}`)
         }
 
-        // Catch if hours are over mins! 
-        minsDiffDec = value - roundedSigHours
-        // convert into 60mins/sec
-        mins = minsDiffDec*60
-        // Truncate the mins 
-        mins = Math.trunc(mins)
-        // Catches any hours not over 24! 
-        hoursDiff = Math.trunc(roundedSigHours)
+        // Has to be UNDER
+        else if (roundedSigHours < 24) {
+            // Catch if hours are over mins! 
+            minsDiffDec = roundedSigHours - Math.trunc(roundedSigHours)
+            // convert into 60mins/hr
+            mins = minsDiffDec*60
+            // Truncate the mins 
+            console.log(`This prints the truncated ${mins} to ${Math.trunc(mins)}`)
+            mins = Math.trunc(mins)
+            // Catches any hours not over 24! 
+            hoursDiff = Math.trunc(roundedSigHours)
+        }
 
-        setCalculator((prevState)=> {
+        setCalculator((prevState) => {
             // ** TODO: stateData for hours not appearing in console! 
             return { days: days, hours: hoursDiff, mins: mins, secs: 0 };
         })
@@ -78,12 +93,12 @@ const CalculateButton = ({ text, calculate }) => {
         // If roundedSigHours > 24 
         if (parameter > 24) {
             // take Difference 
-            days = Math.trunc(parameter/24)
+            days = Math.trunc(parameter / 24)
             console.log(`The number of days is ${days}`)
-            hoursDiffDec = (parameter/24) - days
+            hoursDiffDec = (parameter / 24) - days
             console.log(`The number of hours remaining is ${hoursDiffDec}`)
             // then multiple by 24hrs/day
-            hoursDiff = hoursDiffDec*24
+            hoursDiff = hoursDiffDec * 24
             hoursDiff = Math.trunc(hoursDiff)
             console.log(`The number of hours remaining CONVERTED is ${hoursDiff}`)
         }
